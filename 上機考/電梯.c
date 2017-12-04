731a2002844e78d6ffeef6f4ade26466a7467b14
TMineCola <s104213083@mail1.ncnu.edu.tw>
2017-12-04 14:35:12 +0800

    Judge 1 update

---
+++ 上機考/電梯.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+// 資料陣列, 總人數, 單趟上限
+void routePlan(int input[], int num, int max) {
+    int sum = 0, count = num;
+    // 搭滿的情況
+    for(int i = num - 1; i >= 0; i-=max) {
+        sum += (input[i] - 1) * 2;
+    }
+    // 非搭滿的情況
+    if(count / max == 0 && count % max != 0) {
+        sum += (input[count - 1] - 1) * 2;
+    }
+    printf("%d", sum);
+}
+
+// 資料陣列, 總人數, 單趟上限
+void insertionSort(int input[], int num, int max) {
+    int temp;
+    for(int i = 1; i < num; i++) {
+        temp = input[i];
+        for(int j = i - 1; j >= 0; j--) {
+            if(j < 0) {
+                break;
+            }
+            if(temp < input[j]) {
+                input[j + 1] = input[j];
+                input[j] = temp;
+            }
+        }
+    }
+    routePlan(input, num, max);
+}
+
+int main() {
+    // n = 單趟上限, m = 總人數
+    int n, m;
+    scanf("%d %d", &n, &m);
+    //Person to floor
+    int pf[m];
+    for(int i = 0; i < m; i++) {
+        scanf("%d", &pf[i]);
+    }
+    insertionSort(pf, m, n);
+}
